a7db46eb48911dd1e9bf960b37f7855003344a6a
Ilshat Sultanov <just.sultanov@gmail.com>
2021-02-09 14:12:41 +0300

    Add sample file

---
+++ src/foo/bar/baz.clj
@@ -0,0 +1,15 @@
+(  ns
+  foo.bar.baz  "some doc"
+  (:require (foo.bar [abc :as abc]
+              def))
+  (:use foo.bar.qux)
+  (:import foo.bar.qux.Foo
+    ;; Need this for the thing
+    foo.bar.qux.Bar)
+  )
+
+(defn hello "says hi" (
+                       [] (hello "world")
+                       ) ([name]
+                          ( println "Hello," name  )
+                          ))
